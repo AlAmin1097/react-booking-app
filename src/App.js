7a25f0961d6504e18305b2b585ffd88cd66313b0
AlAmin1097 <amin15-1097@diu.edu.bd>
2022-06-09 07:08:49 +0600

    re-uploaded

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+import "./screenshots/screenshot-react-booking-1.png"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/hotels" element={<List/>}/>
+        <Route path="/hotels/:id" element={<Hotel/>}/>
+        <Route path="*" element={<div>Page Not Found!</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
